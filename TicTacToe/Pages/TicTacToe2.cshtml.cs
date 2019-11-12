3172acfa6d6d18beb1a04dad9a471f965293423d
Aidan Smith <aidansmith1243@gmail.com>
2019-11-12 15:28:30 -0600

    Added TicTacToe2 file

--- TicTacToe/Pages/TicTacToe2.cshtml.cs
+++ TicTacToe/Pages/TicTacToe2.cshtml.cs
@@ -7,11 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TicTacToe.Pages
 {
-    public class TicTacToeModel : PageModel
+    public class TicTacToe2Model : PageModel
     {
+        public char[,] Board { get; } = new char[3, 3];
         public void OnGet()
         {
-            
+            Board[0, 0] = 'X';
         }
     }
 }
